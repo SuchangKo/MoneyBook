26ef086a5c9b611df41b2abb1b31fd3d0c42990b
SuChangKo <ko9459@gmail.com>
2013-03-22 22:05:09 +0900

    create Activity

---
+++ src/com/suchangko/moneybook/Splash.java
@@ -0,0 +1,16 @@
+package com.suchangko.moneybook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
