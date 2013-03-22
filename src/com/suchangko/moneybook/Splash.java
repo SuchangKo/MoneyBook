3585162d4432f96a5cbd0f05dc89cfdd39b08f15
SuChangKo <ko9459@gmail.com>
2013-03-22 22:20:32 +0900

    Splash

--- src/com/suchangko/moneybook/Splash.java
+++ src/com/suchangko/moneybook/Splash.java
@@ -2,6 +2,8 @@ package com.suchangko.moneybook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 public class Splash extends Activity {
 
@@ -9,8 +11,17 @@ public class Splash extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	
+	    setContentView(R.layout.activity_splash);
+	    init();
 	    // TODO Auto-generated method stub
 	}
-
+	private void init(){
+		Handler h = new Handler(){
+			@Override
+			public void handleMessage(Message msg){
+					finish();
+			}
+		};
+		h.sendEmptyMessageDelayed(0,2300);
+	}
 }
