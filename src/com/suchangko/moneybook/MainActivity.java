5e828813f35e2284cf4cec5a6a7cd2fdccfe1309
SuChangKo <ko9459@gmail.com>
2013-03-24 03:08:24 +0900

    Move IntelliJ

--- src/com/suchangko/moneybook/MainActivity.java
+++ src/com/suchangko/moneybook/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends TabActivity{
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+        public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
