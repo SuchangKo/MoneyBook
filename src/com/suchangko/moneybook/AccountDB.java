f78f0fe076d97a773b2d95892b0c77daecae2d81
SuChangKo <ko9459@gmail.com>
2013-03-26 20:04:24 +0900

    Start...

---
+++ src/com/suchangko/moneybook/AccountDB.java
@@ -0,0 +1,68 @@
+package com.suchangko.moneybook;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AccountDB {
+   private DBHelper mHelper;
+   private SQLiteDatabase db;
+   private static final String Database_Name="accountdb";
+   private static final int Database_Version = 2;
+   private static String SQL_Table_Create;
+   private static String Table_Name;
+
+   public static final String SQL_Create_accountdb =
+           "create table accountdb (_id integer primary key autoincrement,"
+                   +"id text not null,"
+                   +"pw text not null"
+                   +")";
+   private final Context cxt;
+
+   private static class DBHelper extends SQLiteOpenHelper {
+       DBHelper(Context context) {
+           super(context,Database_Name,null,Database_Version);
+       }
+       @Override
+       public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_Create_accountdb);
+       }
+       @Override
+       public void onOpen(SQLiteDatabase db) {
+           super.onOpen(db);
+       }
+       @Override
+       public void onUpgrade(SQLiteDatabase db, int i, int i2) {
+           db.execSQL("DROP TABLE IF EXISTS " + Table_Name);
+           onCreate(db);
+       }
+   }
+    public AccountDB(Context cxt, String sql, String tname){
+        this.cxt=cxt;
+        SQL_Table_Create=sql;
+        Table_Name=tname;
+    }
+    public AccountDB open() throws SQLiteException{
+        mHelper = new DBHelper(cxt);
+        db = mHelper.getWritableDatabase();
+        return this;
+    }
+    public void close() {
+        mHelper.close();
+    }
+    public long insertTable(ContentValues values) {
+        return db.insert(Table_Name, null, values);
+    }
+    public boolean deleteTable(String pkColumn, long pkData) {
+        return db.delete(Table_Name, pkColumn+"="+pkData, null)>0;
+    }
+    public Cursor selectTable(String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+        return db.query(Table_Name, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+    public boolean updateTable(ContentValues values, String pkColumn, long pkData) {
+        return db.update(Table_Name, values, pkColumn+"="+pkData, null)>0;
+    }
+}
--- src/com/suchangko/moneybook/LoginActivity.java
+++ src/com/suchangko/moneybook/LoginActivity.java
@@ -1,17 +1,40 @@
 package com.suchangko.moneybook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity {
-
-	/** Called when the activity is first created. */
+public class LoginActivity extends Activity implements View.OnClickListener {
+	Button bt_join;
+    Button bt_login;
+    EditText edit_id;
+    EditText edit_password;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_login);
-	
-	    // TODO Auto-generated method stub
-	}
+	    bt_join=(Button)findViewById(R.id.button_join);
+        bt_login=(Button)findViewById(R.id.button_login);
+        bt_join.setOnClickListener(this);
+        bt_login.setOnClickListener(this);
+        edit_id = (EditText)findViewById(R.id.edit_id);
+        edit_password = (EditText)findViewById(R.id.edit_password);
+    }
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.button_login){
+            String str_id = edit_id.getText().toString();
+            String str_pw = edit_password.getText().toString();
+            Toast.makeText(getApplicationContext(),str_id,Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+        }else if(view.getId()==R.id.button_join){
+
+        }
+    }
 
 }
