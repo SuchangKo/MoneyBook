a01e9a510ec894913ca4605c4f9aa63415d7e07c
SuChangKo <ko9459@gmail.com>
2013-05-16 13:16:46 +0900

    backup

--- src/com/suchangko/moneybook/Tab_ThreeActivity.java
+++ src/com/suchangko/moneybook/Tab_ThreeActivity.java
@@ -2,6 +2,7 @@ package com.suchangko.moneybook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.Button;
 
 /**
@@ -18,4 +19,16 @@ public class Tab_ThreeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab3);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+    	menu.add(0,0,0,"예산 추가").setIcon(R.drawable.ic_menu_add);
+    	menu.add(0,1,0,"예산 복사").setIcon(android.R.drawable.ic_menu_share);
+    	menu.add(0,2,0,"예산 붙여넣기").setIcon(android.R.drawable.ic_menu_set_as);
+    	menu.add(1,3,0,"상위 예산 분배").setIcon(android.R.drawable.ic_menu_search);
+    	menu.add(1,4,0,"도움말").setIcon(android.R.drawable.ic_menu_help);
+    	menu.add(1,5,0,"환경설정").setIcon(R.drawable.ic_menu_more);	
+	return true;
+	}
+    
 }
